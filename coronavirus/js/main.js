b4bdc282af7872b45fa7cbe754957661e4dd2157
lime-sklad <emil.gasanovua@gmail.com>
2020-04-04 14:14:55 +0400

    covid-19
    covid-19 ma

---
+++ coronavirus/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+		// $.ajax({
+		//     headers : { 'Access-Control-Allow-Origin' : '*' },
+		//     type: 'POST',			
+		// 	url: 'https://opendata.ecdc.europa.eu/covid19/casedistribution/json/',
+		//     crossDomain: true,
+		//     contentType: "application/json",
+		//     dataType: 'jsonp',
+		//      success: function(json){
+		//          console.log(json);
+		//      }
+		// });
+
+
+
+// $.ajax({
+//   url: "https://opendata.ecdc.europa.eu/covid19/casedistribution/json/",
+//   beforeSend: function( xhr ) {
+//     xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+//   }
+// })
+//   .done(function( data ) {
+//     if ( console && console.log ) {
+//       console.log( "Sample of data:", data.slice( 0, 100 ) );
+//     }
+//   });
+		
+
+
+
+	$.ajax({			
+    headers : { 'Access-Control-Allow-Origin' : '*' },
+    type: 'POST',			
+	url: 'https://opendata.ecdc.europa.eu/covid19/casedistribution/json/',
+    crossDomain: true,
+    contentType: "application/json",
+    dataType: 'jsonp',
+	    success: function(data){
+
+		// var year = 'AZE', countriesAndTerritories;
+		// data.records.some(function(el) {
+		//    if (el.geoId == "AF") countriesAndTerritories = el.countriesAndTerritories;
+		//    return countriesAndTerritories;
+		// })
+		// console.log(countriesAndTerritories);
+
+	 var year = 'AZE', countriesAndTerritories;
+	 data.records.some(function(el) {
+	    if (el.geoId == "AZ") {
+	    	countriesAndTerritories = el.countriesAndTerritories;
+	    	dateRep = el.dateRep;
+	    }
+	    return countriesAndTerritories;
+	 });
+	 console.log(countriesAndTerritories + dateRep);
+
+		}
+
+	});
+
+
+
+
+});
